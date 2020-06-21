010240bfb1309634f822b07dccebde61d3ddee14
gr8greet <43990056+gr8greet@users.noreply.github.com>
2020-06-21 17:20:09 +0530

    Update Ch6-Pipeline_cheatsheet.ps1

--- Ch6-Pipeline_cheatsheet.ps1
+++ Ch6-Pipeline_cheatsheet.ps1
@@ -1,7 +1,7 @@
 Get-Help Get-Process -Parameter * | Where-Object {$_.pipelineInput -Like 'true*'} | Select-Object Name, PipelineInput
 What does the $ _ mean in the following Windows PowerShell expression?
 Get-Service | Where-Object { $_.Status –eq "Stopped" }
-c. Current item in the pipeline
+Ans: Current item in the pipeline
 The $_ automatically created variable is used as a shorthand method for iterating through each object in a collection. Actually, the $_ shortcut is synonymous with the $PSItem automatic variable. Therefore, we can rewrite the previous statement like this:
 Get-Service | Where-Object { $PSItem.Status –eq "Stopped" }
 
